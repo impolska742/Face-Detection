d85a0dfce48746213024bb0ea444654223c10364
Vaibhav Bhardwaj <vaibhav19bhardwaj@gmail.com>
2021-01-18 14:13:04 +0530

    Face Detection Python Script using OpenCV

---
+++ face-detect.py
@@ -0,0 +1,23 @@
+import cv2
+
+cap = cv2.VideoCapture(1)
+classifier = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
+while True:
+
+    ret, frame = cap.read()
+
+    if ret:
+        faces = classifier.detectMultiScale(frame)
+
+        for face in faces:
+            x, y, w, h = face
+            frame = cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 10)
+            cv2.imshow("My Window", frame)
+
+    key = cv2.waitKey(1)
+
+    if key == ord("q"):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
